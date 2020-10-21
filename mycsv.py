f4bd7447232f688329e3177dac834ab9a38a6454
OverScanf <72699237+OverScanf@users.noreply.github.com>
2020-10-21 15:28:46 +0200

    Update mycsv.py
    Adding comments

--- mycsv.py
+++ mycsv.py
@@ -2,13 +2,13 @@
 
 # mycsv.py
 
-def read_csv(path_to_csv_file, delimiter=","): #<FINISHED>
+def read_csv(path_to_csv_file, delimiter=","): 
     try:
         res = list()
         fileToRead = open(path_to_csv_file,"r")
         for line in fileToRead.readlines():
-            res.append(line.split(delimiter)) #on separe avec le délimiteur
-        for i in range(len(res)): #on supprime le guillemet dans les entrées
+            res.append(line.split(delimiter)) 
+        for i in range(len(res)): 
             for j in range(len(res[i])):
                 res[i][j] = res[i][j].replace("\"","")
                 res[i][j] = res[i][j].replace("\n","")
@@ -17,14 +17,14 @@ def read_csv(path_to_csv_file, delimiter=","): #<FINISHED>
     except:
         print("Error, such file doesn't exist")
 
-def write_csv(path_to_csv_file, data, delimiter=','): #<FINISHED>
+def write_csv(path_to_csv_file, data, delimiter=','): 
     try:
         fileToWrite = open(path_to_csv_file,"w")
         for line in data:
-            for i in range(len(line)): #tant qu'il a une entrée à ajouter sur la ligne
-                if i != (len(line) - 1): #ce n'est pas le dernier element de la ligne
+            for i in range(len(line)): # while there is an entry to add
+                if i != (len(line) - 1): # it's no the last element on the line
                     fileToWrite.write(line[i] + delimiter)
-                else: #c'est bien le dernier element de la ligne donc on met pas de séparateur mais un saut de ligne
+                else: # It is the last element on the line so we add a new line jump instead of a separtor
                     fileToWrite.write(line[i] + "\n")
         fileToWrite.close()
     except:
